dbd3902cf00c36e006d9a54d8415b35b314351a9
doroshdev0111 <doroshdev0111@gmail.com>
2023-07-26 13:36:47 -0400

    authentication set public

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const app = require('./config/express');
 require('./config/mongoose');
 
 // module.parent check is required to support mocha watch
-// src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
